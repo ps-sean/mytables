e1441d530561c4f107e423c42f695a9dd62b6a7a
Sean Ross <sean@str94.co.uk>
2021-04-12 21:54:30 +0100

    use tls for echo server

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,6 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: true,
+    forceTLS: process.env.MIX_ECHO_TLS,
     disableStats: true,
 });
