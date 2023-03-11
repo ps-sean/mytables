edfd59a086acfb452256b3cac9632ebd56171a9b
Sean Ross <sean@str94.co.uk>
2023-03-11 23:07:28 +0000

    Updated to deployer 7

--- deploy.php
+++ deploy.php
@@ -2,65 +2,33 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'vendor/deployer/recipes/recipe/slack.php';
-require 'vendor/deployer/recipes/recipe/npm.php';
 
-// Project name
-set('application', 'myTables');
+// Config
 
-// Project repository
-set('repository', 'https://str94@bitbucket.org/str94/mytables.git');
+set('repository', 'https://github.com/ps-sean/mytables.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-
-//only keep last 3 releases
-set('keep_releases', 3);
-
-// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
-
-// Writable dirs by web server
 add('writable_dirs', []);
-
-task('webpack', function () {
-    run("cd {{release_path}} && npm run production");
-});
-
-task('horizon', function () {
-    run("sudo supervisorctl restart horizon");
-});
-
-task('websockets', function () {
-    run("sudo supervisorctl restart websockets");
-});
 
 // Hosts
 
 host('18.133.49.69')
-    ->user('ec2-user')
-    ->identityFile('~/.ssh/mt2.pem')
+    ->set('remote_user', 'ec2-user')
     ->set('deploy_path', '/var/www/html');
 
 // Tasks
 
 task('build', function () {
-    run('cd {{release_path}} && build');
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run production');
 });
 
-// [Optional] if deploy fails automatically unlock.
+// Hooks
+
 after('deploy:failed', 'deploy:unlock');
 
-// Migrate database before symlink new release.
+after('deploy:vendors', 'build');
 
-before('deploy:symlink', 'artisan:migrate');
-
-after('deploy:vendors', 'npm:install');
-
-after('npm:install', 'webpack');
-
-after('deploy:symlink', 'horizon');
-
-after('horizon', 'websockets');
-
+after('deploy:symlink', 'artisan:horizon:terminate');
