8bbc897953f06712fb24c4f17064705ff557898b
Sean Ross <56582799+ps-sean@users.noreply.github.com>
2024-08-13 20:19:25 +0100

    Updated laravel/cashier to 15.x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Restaurant;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 use Stripe\Stripe;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Stripe::setApiKey(config("services.stripe.secret"));
+
+        Cashier::useCustomerModel(Restaurant::class);
 
         Validator::extend("phone", function($attribute, $value, $parameters, $validator){
             return preg_match("^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$^", $value);
---
+++ database/migrations/2024_08_13_185237_rename_stripe_plan_to_stripe_price_column.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->renameColumn('stripe_plan', 'stripe_price');
+        });
+
+        Schema::table('subscription_items', function (Blueprint $table) {
+            $table->renameColumn('stripe_plan', 'stripe_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->renameColumn('stripe_price', 'stripe_plan');
+        });
+
+        Schema::table('subscription_items', function (Blueprint $table) {
+            $table->renameColumn('stripe_price', 'stripe_plan');
+        });
+    }
+};
